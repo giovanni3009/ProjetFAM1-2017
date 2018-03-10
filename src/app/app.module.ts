d649ccd9cfc90ed53b28ff861d8f2688de0aef1c
giovanni3009 <giovanni.manantsoa@gmail.com>
2018-03-10 17:48:25 +0300

    configuration imprevisionnelle

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,15 @@ import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { AccueilPage } from '../pages/accueil/accueil';
 import { ApproximationPage } from '../pages/approximation/approximation';
+import { AproposPage } from '../pages/apropos/apropos';
 
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
     AccueilPage,
-    ApproximationPage
+    ApproximationPage,
+    AproposPage
   ],
   imports: [
     BrowserModule,
@@ -25,7 +27,8 @@ import { ApproximationPage } from '../pages/approximation/approximation';
     MyApp,
     HomePage,
     AccueilPage,
-    ApproximationPage
+    ApproximationPage,
+    AproposPage
   ],
   providers: [
     StatusBar,
